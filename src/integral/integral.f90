8464b89efe42544fe5421803e5d56405c9b1354a
wuhong <xunzhangthu@gmail.com>
2012-08-15 23:28:01 +0800

    fix the NaN bug in integral.f90

--- src/integral/integral.f90
+++ src/integral/integral.f90
@@ -11,36 +11,50 @@
 !
 !---------------------------------------------------------------------------
       
-      
+     
       subroutine integral3d(na, ns, nr, int_res)
       
       use parameter_def
+      use common_data
+      implicit none
+
       ! argument
       integer, intent(in) :: na, ns, nr
       real(double), intent(out) :: int_res
       ! local variables
-      integer :: ka, js, ir, np
-      real(double) :: int_rval(nr), int_sval(ns)
+      integer :: is, ka, js, ir, np
+      integer :: ib, ja
+      real(double) :: int_rval(ns), int_sval(na)
+      ! real(double) :: int_rval(nr), int_sval(ns)
       real(double) :: rcoords(nr), rwgts(nr)
       real(double) :: scoords(3, ns), swgts(ns)
       real(double) :: acoords(3, na)
-      real(double) :: pcoords(3, ns * nr), pwgts(na, ns * nr)
+      !real(double) :: pcoords(3, ns * nr), pwgts(na, ns * nr)
+      real(double) :: pcoords(3, 1202*500), pwgts(18, 1202*500)
       real(double) :: sine
-      real(double) :: coord(3)
-      real(double) :: charx(ns * nr)
+      real(double) :: charx
+      real(double) :: tmp_coord(3)
+      !real(double) :: charx(ns * nr)
+      ! real(double) :: zn_atom
       
+      print *, "I am here"
       np = ns * nr
-
+      
+      ! generate points in radial direction, the nr rcoords is relative coordinates to atom
       call Sec_Gauss_Chebyshev(nr, 1, rcoords, rwgts) 
       
+      ! generate points in angular direction, the ns scoords is relative coordinates to atom
       call lebsam(ns, scoords, swgts) 
-      
+     
+      ! get the coordinates of na atoms, notice raw * rbuff
       do ka = 1, na
-         acoords(1, ka) = grid3d(ka)%x
-         acoords(2, ka) = grid3d(ka)%y
-         acoords(3, ka) = grid3d(ka)%z
+         ja = atomtag(ka)
+         acoords(1, ka) = coord(1, ja)
+         acoords(2, ka) = coord(2, ja)
+         acoords(3, ka) = coord(3, ja)
       end do
-
+      
+      ! to each atom, still the relative coordinates, but combine radial and angular
       do ir = 1, nr
          do is = 1, ns
             pcoords(1, (ir - 1) * ns + is) = rcoords(ir) * scoords(1, is) 
@@ -58,24 +72,24 @@
             int_rval(js) = 0.0d0
             do ir = 1, nr
                sine = 0.0d0
-               coord(1) = 0.0d0
-               coord(2) = 0.0d0
-               coord(3) = 0.0d0
-               coord(1) = rcoords(ir) * scoords(1, is) 
-               coord(2) = rcoords(ir) * scoords(2, is)
-               coord(3) = rcoords(ir) * scoords(3, is)
-               sine = coord(3) / rcoords(ir)
-               sine = sqrt(1 - tmp ** 2)
+               tmp_coord(1) = 0.0d0
+               tmp_coord(2) = 0.0d0
+               tmp_coord(3) = 0.0d0
+               tmp_coord(1) = rcoords(ir) * scoords(1, js) 
+               tmp_coord(2) = rcoords(ir) * scoords(2, js)
+               tmp_coord(3) = rcoords(ir) * scoords(3, js)
+               sine = tmp_coord(3) / rcoords(ir)
+               sine = sqrt(1 - sine ** 2)
                
-               call interp(ka, coord, charx)
-               
+               call interp(ka, tmp_coord, charx)
                ! or F(p(coord))
-               !int_rval(js) = int_rval(js) + rwgts(ir) * ( pwgts(ka, (ir - 1) * ns + is) * F(p(ir, is)) * sine ) 
-               int_rval(js) = int_rval(js) + rwgts(ir) * ( pwgts(ka, (ir - 1) * ns + is) * charx * sine ) 
+               ! int_rval(js) = int_rval(js) + rwgts(ir) * ( pwgts(ka, (ir - 1) * ns + is) * F(p(ir, is)) * sine ) 
+               int_rval(js) = int_rval(js) + rwgts(ir) * &
+                              ( pwgts(ka, (ir - 1) * ns + js) * charx * sine) 
             end do
             int_sval(ka) = int_sval(ka) + swgts(js) * int_rval(js)
          end do
          int_res = int_res + int_sval(ka)
       end do 
-      
+
       end subroutine integral3d
